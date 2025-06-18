07d2e01873468df9e03a164b7916edea124711c5
shiinawhitee <shiinawhitee@gmail.com>
2025-06-18 19:21:28 +0800

    还原

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -283,7 +283,7 @@ def generate_cache_key(chat_request, last_n_messages: int = 65536, is_gemini=Fal
                         h.update(b'image_url:') # 加入类型标识符
                         if image_data.startswith('data:image/'):
                             # 对于base64图像，使用前32字符作为标识符
-                            h.update(image_data[:256].encode('utf-8'))
+                            h.update(image_data[:32].encode('utf-8'))
                         else:
                             h.update(image_data.encode('utf-8'))
 
